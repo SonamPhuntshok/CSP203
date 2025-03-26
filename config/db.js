def009fe58a4bd835bcd3c21616e6e91dcfe8c05
SonamPhuntshok <Sonammmm2001@gmail.com>
2025-03-26 21:36:36 +0600

    Practical 3

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 const db = pgp({
     host: process.env.DB_HOST || 'localhost', 
-    port: 5432, database: process.env.DB_NAME || 'bookstore_db', 
+    port: 5432, 
+    database: process.env.DB_NAME || 'bookstore_db', 
     user: process.env.DB_USER || 'postgres', 
     password: process.env.DB_PASS || 'yourpassword' 
 });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+
 router.post("/signup", authController.registerUser);
 router.post("/login", authController.loginUser);
 router.get("/logout", authController.logoutUser);
+
 router.get("/signup", (req, res) => {
+  console.log('signup route hit');
   res.render("signup");
 });
 router.get("/login", (req, res) => {
